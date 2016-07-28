81d027603a56fcc00ecd371895bb593374e1cc43
Dylan Kaufman <dylankau@gmail.com>
2016-07-27 23:57:28 -0400

    this compiles, did some simplify

--- risch.h
+++ risch.h
@@ -20,16 +20,17 @@ class funk{
 
 	//~funk();
 
-	funk& operator=(const funk& obj)
+	funk& operator=(const funk& obj);
 	//funk& operator=(const funk&& obj);
 
-	funk& operator+(const funk& obj);	
-	funk& operator*(const funk& obj);
-	funk& operator/(const funk& obj);	
+	funk operator+(const funk& obj);	
+	funk operator*(const funk& obj);
+	funk operator/(const funk& obj);	
 
 	bool operator==(const funk& obj);
 	bool statesAndNodesEqual(const funk& obj);
 	bool compareWithoutCoef(const funk& obj);
+	bool compareMathEquiv(const funk&);
 	//need some way to incoroprate functions like sin, cos, tan.
 	//need some way to find neccisary functions...
 
@@ -51,6 +52,7 @@ class funk{
 	void intoReady();
 	void degOrg();
 	void degOrg(char c);
+	bool isConstant();
 	
 	//holds protostate during parsing
 	std::string pstring;
@@ -64,6 +66,17 @@ class funk{
 	
 	//divide numer A / denom B
 	//sinngle nodes go to A
+
+ private:
+	void replaceWith(const funk&);
+	void replaceWith(const std::unique_ptr<funk>&);
+	void breakExpo();
+	void evaluateCoef(int, std::unique_ptr<funk>&);
+	void multiplyDivisions();
+	void setBase();
+	void simplifyAddition();
+	void simplifyMulitiplication();
+	void simplifyDivision();
 	
 };
 
